5d898468e56193b35847226c41707f503a0aec9f
anosu <jitsu233@gmail.com>
2023-01-23 17:22:29 +0800

    change js

---
+++ docs/.vuepress/public/js/main.js
@@ -0,0 +1,18 @@
+let secs = parseInt((new Date().getTime() - new Date('2022-12-21 23:26:10').getTime()) / 1000)
+let time = {
+    day: Math.floor(secs / 86400),
+    hour: Math.floor(secs % 86400 / 3600),
+    min: Math.floor(secs % 86400 % 3600 / 60),
+    sec: secs % 86400 % 3600 % 60
+}
+let xmlhttp;
+xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        let features = document.getElementsByClassName('feature')
+        features[4].children[1].innerText = time.day + '天' + time.hour + '小时' + time.min + '分钟' + time.sec + '秒'
+        features[5].children[1].innerText = '自2023.1.21：' + xmlhttp.responseText + '次'
+    }
+}
+xmlhttp.open("GET", "https://image.anosu.top/pixiv/count", true);
+xmlhttp.send();
